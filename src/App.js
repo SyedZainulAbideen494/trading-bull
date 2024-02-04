775b449721eb1b0aae804f41beb8f22f32df881b
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-02-04 15:14:49 +0530

    m

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { useRef, Fragment, useState, useCallback } from 'react';
 import './App.css'
 import logo from './images/saz-infotech-w-removebg-preview.png'
 import banner from './images/Untitled design.png'
-
+import code from './images/icons8-code-50.png'
+import maintaion from './images/icons8-development-50.png'
+import design from './images/icons8-design-50.png'
+import upgrade from './images/icons8-update-50.png'
 
 const App = () => {
    return(
@@ -33,6 +36,34 @@ const App = () => {
         </div>
       </section>
       <section className='hero-section-img'></section>
+    </div>
+    <div className='section-1'>
+        <section className='section-1-textarea'>
+            <h2>Our Work Offers</h2>
+            <button>Services</button>
+        </section>
+        <section className='section-1-offers'>
+            <div className='offer-section-1'>
+                <img src={design}/>
+                <h3>Design</h3>
+                <p>We design your website from scratch</p>
+            </div>
+            <div className='offer-section-1'>
+                <img src={code}/>
+                <h3>Development</h3>
+                <p>We develop your website from scratch</p>
+            </div>
+            <div className='offer-section-1'>
+                <img src={maintaion}/>
+                <h3>manage</h3>
+                <p>We manage and maintane your website after development</p>
+            </div>
+            <div className='offer-section-1'>
+                <img src={upgrade}/>
+                <h3>Update</h3>
+                <p>We update your website from time to time</p>
+            </div>
+        </section>
     </div>
     </div>
    </Fragment>
