742ae7c7327083ac47e8f8319caa14de856330ca
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-02-04 13:16:31 +0530

    m

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import logo from './images/saz-infotech-w-removebg-preview.png'
 const App = () => {
    return(
    <Fragment>
-         <div className='main-div'>
-        <div className='header'>
-            <header>
-                <div className='logo-header'>
-                    <img src={logo}/>
-                </div>
-                <div className='header-btns'>
-                    <button>About us</button>
-                    <button>Our work</button>
-                    <button>services</button>
-                    <button>Contact us</button>
-                </div>
-            </header>
-        </div>
+     <div className='main-div'>
+      <div className='header'>
+        <header>
+          <div className='logo-header'>
+            <img src={logo} alt="Logo" />
+          </div>
+          <div className='header-btns'>
+            <button>About us</button>
+            <button>Our work</button>
+            <button>Services</button>
+            <button className="contact-button" style={{border: '1px solid black', borderRadius: '25px', padding: '10px 20px'}}>Contact us</button>
+          </div>
+        </header>
+      </div>
     </div>
    </Fragment>
    )
